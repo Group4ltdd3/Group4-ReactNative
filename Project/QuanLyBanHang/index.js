4c5770bda05827ed6a00442bcd3b60a641625b2a
Quan Vo <33823448+nhocnho1993@users.noreply.github.com>
2018-11-14 07:46:07 +0700

    Test du lieu sau khi tao project

--- Project/QuanLyBanHang/index.js
+++ Project/QuanLyBanHang/index.js
@@ -3,5 +3,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Item from './Components/Item'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Item);
