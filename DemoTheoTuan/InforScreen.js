32f52ebd5dd4d354b191d4c4652222839cc99472
Quan Vo <33823448+nhocnho1993@users.noreply.github.com>
2018-12-09 22:49:49 +0700

    update InfoScreen

--- DemoTheoTuan/InforScreen.js
+++ DemoTheoTuan/InforScreen.js
@@ -1,76 +1,116 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Button} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
 
-export default class InforScreen extends Component{
+export default class InforScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            mang: []
+
+
+        }
+
+
+    }
     static navigationOptions = {
         header: null // Không sử dụng thanh toolbar giống trong Android
-      };
-    
-    render(){
-        return(
-          
+    };
+
+    render() {
+        return (
+
             <View style={styles.container}>
-        
-            <View>
-                <TouchableOpacity style={styles.buttonContainer}>
-                    <Text style={styles.buttonText}>User Informations</Text>
-                </TouchableOpacity>
-                <Image style= {styles.imageStyle}
-                    source={require('./images/userviet.png')}
+
+                <View>
+                    <TouchableOpacity style={styles.buttonContainer}>
+                        <Text style={styles.buttonText}>User Informations</Text>
+                    </TouchableOpacity>
+                    <Image style={styles.imageStyle}
+                        source={require('./images/userviet.png')}
                     //style={{flexWrap: 'wrap', width: 150, height:150}}                    
-                />
-                
-                <Text style={styles.textTen}>Username : thai</Text>
-                <Text style={styles.textEmail}>Email : thai@gmail.com</Text>
-                            
+                    />
+
+                    <Text style={styles.textTen}>Username : thai</Text>
+                    <Text style={styles.textEmail}>Email : thai@gmail.com</Text>
+
+                </View>
+                <TouchableOpacity style={styles.buttonContainerLogout} onPress={() => this.logOut()}>
+                    <Text style={styles.buttonLogout}> Logout</Text>
+                </TouchableOpacity>
             </View>
-            </View>
-                       
+
+
         )
     }
+
+    logOut() {
+        alert('Logout Successfully');
+        this.props.navigation.navigate('Login');
+    }
+
+
+
 }
 styles = StyleSheet.create({
     container: {
         flex: 1,
-       // backgroundColor: 'rgb(32, 53, 70)', 
-       backgroundColor: "#efe2a6",
+        // backgroundColor: 'rgb(32, 53, 70)', 
+        backgroundColor: "#efe2a6",
         flexDirection: 'column',
-      },
-    buttonContainer:{
-        backgroundColor:'#16a085',
+    },
+    buttonContainer: {
+        backgroundColor: '#16a085',
         paddingVertical: 20,
-        
+
     },
-    textTen:{
-        marginTop:10,
-        fontSize:20,
+    textTen: {
+        marginTop: 10,
+        fontSize: 20,
         textAlign: 'center'
     },
-  
-    textEmail:{
-        marginTop:10,
-        fontSize:20,
+
+    textEmail: {
+        marginTop: 10,
+        fontSize: 20,
         textAlign: 'center'
     },
-    
-    imageStyle:{
-        width:150, height:150,
-        marginBottom:20,
-        marginLeft:102,
-        marginTop:20,     
+
+    imageStyle: {
+        width: 100, 
+        height: 100,
+        marginBottom: 20,
+        marginLeft: 140,
+        marginTop: 20,
     },
-    textname:{
-        fontSize:23,
+    textname: {
+        fontSize: 23,
         textAlign: 'center',
-        marginTop:5,
-        opacity:0.9
+        marginTop: 5,
+        opacity: 0.9
     },
-    
-    
-    buttonText:{
-    textAlign:'center',
-    color: 'rgb(32,53,70)',
-    fontWeight:'bold',
-    fontSize:18
+
+
+    buttonText: {
+        textAlign: 'center',
+        color: 'rgb(32,53,70)',
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    buttonContainerLogout: {
+        backgroundColor: '#6193ab',
+        paddingVertical: 15,
+        borderRadius: 30,
+        height: 40,
+        width: 150,
+        marginTop: 10,
+        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 120
+
+    },
+    buttonLogout: {
+        color: '#FFFFFF'
     }
 })
